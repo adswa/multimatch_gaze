fcf385806f8e00e685b23c767d414ce916408549
Adina Wagner <adina.wagner@t-online.de>
2018-10-01 21:40:50 -0400

    copy the directory for unit tests into repo

---
+++ tests/test_MultiMatch.py
@@ -0,0 +1,76 @@
+import pytest
+import numpy as np
+from . import utils as ut
+from .. import MultiMatch as M
+
+def test_same_real_data(run = 1, subj = 1):
+    data1, data2, shots = ut.same_sample(run, subj)
+    segments = M.doComparison(shots, data1, data2)
+    segmentfinal = np.array(segments)
+    assert np.all(segmentfinal.all(1))
+
+def test_StructureGeneration(length = 5):
+    fix_vector = ut.mk_fix_vector(length)
+    results = M.generateStructureArrayScanpath(fix_vector)
+    assert len(results[0]) == len(fix_vector)
+    assert len(results[1]) == len(fix_vector)
+    assert len(results[2]) == len(fix_vector)
+    assert len(results[3]) == len(fix_vector)-1
+    assert len(results[4]) == len(fix_vector)-1
+    assert len(results[5]) == len(fix_vector)-1
+    assert len(results[6]) == len(fix_vector)-1
+    assert len(results[7]) == len(fix_vector)-1
+    assert len(results[8]) == len(fix_vector)-1
+
+def test_calVectorDifferences(length = 5):
+    data1, data2 = ut.mk_strucArray(length=5)
+    Matrix = M.calVectordifferences(data1, data2)
+    assert Matrix.shape[0] == length -1
+    assert Matrix.shape[1] == 2*length -2
+
+
+def test_anglesim():
+    M_assignment, path, angles1, angles2, angles3, angles4 = ut.mk_angles()
+    a1a2 = M.calAngularDifference(angles1, angles2, path, M_assignment)
+    a3a4 = M.calAngularDifference(angles3, angles4, path, M_assignment)
+    assert a1a2 == [0, 1.0459999999999994, 1.5700000000000003, 2.08,
+    3.1400000000000006]
+    assert a3a4 == [3.1400000000000006, 2.0971853071795863, 1.5711853071795865,
+    1.0491853071795862, 0.0031853071795859833]
+
+def test_closestright():
+    mylist = np.arange(0, 25)
+    for i in range(0, len(mylist)):
+        res = M.takeClosestright(mylist, i)
+        if i == 0:
+            assert res == 1
+        elif i == len(mylist)-1:
+            assert res == 24
+        else:
+            assert res == i+1
+
+
+def test_createChunks(run=1, subj=1):
+    data1, data2, shots = ut.same_sample(run, subj)
+    onsets = M.createOnsets(shots, 3)
+    fixations = M.preprocess(data1)
+    startid, endid = M.createChunks(onsets, fixations, 3)
+    assert len(startid) == len(endid)
+    trues = []
+    for i in range(0, len(startid)):
+        trues.append(startid[i]<=endid[i])
+    assert all(trues)
+
+
+def test_durationsim():
+    M_assignment, path, duration1, duration2 = ut.mk_durs()
+    res = M.calDurationDifference(duration1, duration2, path, M_assignment)
+    assert all(np.asarray(res)>= 0)
+
+
+def test_longshot():
+    shots = ut.short_shots()
+    newshots = M.longshot(shots, dur = 4.92)
+    assert len(newshots) == len(shots)-2
+
+
---
+++ tests/utils.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+import os.path
+import random
+
+def same_sample(run = 1, subj = 1):
+    '''duplicate dataset to force exactly similar scanpaths. Choose the run
+    (integer between 1-8) and whether you want a lab (1) or mri (2) subject'''
+    if subj == 1:
+        sub = "sub-30"
+    else:
+        sub = "sub-10"
+    path = os.path.join("/home/adina/Documents/MastersThesis/Alles/code/tests/testdata",
+    "{}_task-movie_run-{}_events.tsv".format(sub, run))
+    loc = os.path.join("/home/adina/Documents/MastersThesis/Alles/code/tests/testdata", "locations_run-{}_events.tsv".format(run))
+    data = np.recfromcsv(path, delimiter= '\t', dtype={'names':('onset',
+            'duration', 'label', 'start_x', 'start_y', 'end_x', 'end_y', 'amp',
+            'peak_vel', 'med_vel', 'avg_vel'), 'formats':('f8', 'f8', 'U10',
+            'f8', 'f8', 'f8','f8', 'f8', 'f8', 'f8', 'f8')})
+    data2 = data
+    shots = pd.read_csv(loc, sep = '\t')
+    return data, data2, shots
+
+def short_shots(run=3):
+    '''create a shortened shots location annotation to test longshots()'''
+    loc = os.path.join("/home/adina/Documents/MastersThesis/Alles/code/tests/testdata",
+    "locations_run-{}_events.tsv".format(run))
+    shots = pd.read_csv(loc, sep = '\t')
+    shortshots = shots[0:20]
+    return shortshots
+
+
+def mk_fix_vector(length = 5):
+    '''creates a random length x 3 fixation vector in form of a record array'''
+    fix = np.recarray((0,), dtype=[('start_x', '<f8'), ('start_y', '<f8'),
+    ('duration', '<f8')])
+    for i in range(0, length):
+        fixation = np.array((np.random.uniform(1, 720), np.random.uniform(1, 720),
+        np.random.uniform(0.01, 5)), dtype = [('start_x', float), ('start_y',
+        float), ('duration', float)])
+        fix = np.append(fix, fixation)
+    return fix
+
+
+def mk_strucArray(length=5):
+    '''create a random scanpath in the data format generateScanpathStructureArray
+    would output'''
+    fixation_x = random.sample(range(700), length)
+    fixation_y = random.sample(range(700), length)
+    fixation_dur = random.sample(range(5), length)
+    saccade_x = random.sample(range(700), length-1)
+    saccade_y = random.sample(range(700), length-1)
+    saccade_lenx = random.sample(range(700), length-1)
+    saccade_leny = random.sample(range(700), length-1)
+    saccade_rho = random.sample(range(700), length-1)
+    saccade_theta = random.sample(range(4), length-1)
+    eyedata = [fixation_x, fixation_y, fixation_dur, saccade_x, saccade_y,
+                saccade_lenx, saccade_leny, saccade_theta, saccade_rho]
+    eyedata2 = [fixation_x[::-1]*2, fixation_y[::-1]*2, fixation_dur[::-1]*2,
+    saccade_x[::-1]*2, saccade_y[::-1]*2, saccade_lenx[::-1]*2, saccade_leny[::-1]*2,
+    saccade_theta[::-1]*2, saccade_rho[::-1]*2]
+    return eyedata, eyedata2
+
+
+def mk_angles():
+    '''creates vectors with predefined angular relations. angles1 and angles2
+    contain the following properties: 1. same 0°, 2. 60° diff, 3. 90° diff,
+    4.120° diff,4. 180° diff (max. dissimilar). They are in sectors (0,1) and
+    (0, -1).
+    Angles3 and angles4 contain the same properties reversed and lie in sectors
+    (-1, 0) and (-1, -1)'''
+    angles1 = [[],[],[],[],[],[],[],[0, 0.523, 0.785, 1.04, 1.57],[]]
+    angles2 = [[],[],[],[],[],[],[],[0, -0.523, -0.785, -1.04, -1.57],[]]
+    angles3 = [[],[],[],[],[],[],[],[1.57, 2.093, 2.356, 2.617, 3.14],[]]
+    angles4 = [[],[],[],[],[],[],[],[-1.57, -2.093, -2.356, -2.617, -3.14],[]]
+    path = [0, 6, 12, 18, 24]
+    M_assignment = np.arange(5*5).reshape(5,5)
+    return M_assignment, path, angles1, angles2, angles3, angles4
+
+
+def mk_durs():
+    '''create some example duration for test_durationsim()'''
+    durations1 =[[],[],[0.001, 20.0, 7, -18, -2.0],[],[],[],[],[],[]]
+    durations2 =[[],[],[0.008, 18.0, 7, -11, 3.0],[],[],[],[],[], []]
+    path = [0, 6, 12, 18, 24]
+    M_assignment = np.arange(5*5).reshape(5,5)
+    return(M_assignment, path, durations1, durations2)
+
