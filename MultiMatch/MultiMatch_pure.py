9ea910555eb4c540d6e912d1dfc713a1aa1c9ff6
Adina Wagner <adina.wagner@t-online.de>
2018-11-30 11:25:20 -0500

    fix screenmeasure default

--- MultiMatch/MultiMatch_pure.py
+++ MultiMatch/MultiMatch_pure.py
@@ -909,7 +909,7 @@ if __name__ == '__main__':
     parser.add_argument('-di', '--direction_threshold', help='direction_threshold: for direction based grouping. If 0: no grouping will be performed', type = float, default=0.0)
     parser.add_argument('-am', '--amplitude_threshold', help='amplitude_threshold: for amplitude based grouping. If 0: no grouping will be performed', type = float, default=0.0)
     parser.add_argument('-du', '--duration_threshold', help='duration_threshold: for direction based grouping.', type = float, default=0.0)
-    parser.add_argument('-sz', '--screensize', help='screensize: Resolution of screen in px, default is [720, 1280]', default = [720, 1280])
+    parser.add_argument('-sz', '--screensize', help='screensize: Resolution of screen in px, default is [1280, 720]', default = [1280, 720])
 
     args = parser.parse_args()
 
