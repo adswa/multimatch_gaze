93e8e792fd9d2cfc3913f0436d33ea641dc8d803
Adina Wagner <adina.wagner@t-online.de>
2018-12-01 22:01:26 -0500

    added starting point for __main__ and __init__

--- MultiMatch/__init__.py
+++ MultiMatch/__init__.py
@@ -1,1 +1,22 @@
+# emacs: -*- mode: python; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 noet:
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+#
+#   See COPYING file distributed along with the multimatch package for the
+#   copyright and license terms.
+#
+# ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
+
 from __future__ import absolute_import
+
+
+__version__ = '0.1'
+
+
+
+import logging
+lgr = logging.getLogger('MultiMatch')
+
+#TODO maybe name change? MultiMatch_pure --> multimatch; MultiMatch --> multimatch_forrest
+
+
