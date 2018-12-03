93d4b27a9018947f266256b1a128332c91023a27
Adina Wagner <adina.wagner@t-online.de>
2018-12-03 18:40:36 -0500

    presumably what needs to be in this header

--- multimatch/__init__.py
+++ multimatch/__init__.py
@@ -2,8 +2,8 @@
 # ex: set sts=4 ts=4 sw=4 noet:
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 #
-#   See COPYING file distributed along with the multimatch package for the
-#   copyright and license terms.
+#   See LICENSE file distributed along with the multimatch package for the
+#   license terms.
 #
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 
