7ee9002ab8d498f2b2e7849251b7600a6de1491f
Adina Wagner <adina.wagner@t-online.de>
2018-12-05 09:28:19 -0500

    more attempts to enable automatic docstring extraction

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -178,10 +178,4 @@ epub_exclude_files = ['search.html']
 
 # -- Extension configuration -------------------------------------------------
 
-# -- automatic docstring extraction ------------------------------------------
-import os
-import sys
 
-sys.path.insert(0, os.path.abspath('../..'))
-extensions = [
-'sphinx.ext.autodoc',]
