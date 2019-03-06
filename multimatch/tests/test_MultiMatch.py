efc8cd89a6af1d6178f611ade55e574cc8e05017
Adina Wagner <adina.wagner@t-online.de>
2019-03-06 03:23:26 +0100

    TST: sort tests, disable superfluous test of now unused multimatch_forrest functions

--- multimatch/tests/test_MultiMatch.py
+++ multimatch/tests/test_MultiMatch.py
@@ -1,17 +1,17 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath('./'))
 import numpy as np
 from . import utils as ut
 from .. import multimatch as mp
 from .. import multimatch_forrest as m
 
 
-def test_same_real_data_forrest(run=1, subj=1):
+def test_same_real_data_forrest(run=1,
+                                subj=1):
     """Test identical forrest scanpaths
 
-    Tests the studyforrest-specific functionality: are two identical scanpaths
-    from the movie identical in all scanpath dimensions? (no grouping)
+    Tests whether two identical scanpaths
+    from the movie identical in all scanpath dimensions(no grouping).
 
     Parameters
     -----------
@@ -41,7 +41,7 @@ def test_same_real_data_forrest(run=1, subj=1):
 def test_same_real_data():
     """Test identical scanpaths
 
-    Tests the pure multimatch functionality: are two identical scanpaths
+    smoketest reading in of fixation vectors, tests whether two identical scanpaths
     supplied as fixation vectors identical in all scanpath dimensions?
     """
     import os
@@ -61,10 +61,11 @@ def test_same_real_data():
     assert np.all(resultsfinal.all(1))
 
 
-def test_simplification(run=1, subj=1):
+def test_simplification(run=1,
+                        subj=1):
     """
     Smoketest to see whether simplification blows up and whether identical
-    scanpaths are still identical after grouping.
+    scanpaths are still identical after grouping, using real data.
     """
     testfile = os.path.abspath('multimatch/tests/testdata/segment_5_sub-19.tsv')
     data1 = np.recfromcsv(testfile,
@@ -159,103 +160,6 @@ def test_durationsim():
     assert all(np.asarray(res) >= 0)
 
 
-def test_closestright():
-    """
-    Tests whether closestright function works as intended and does not
-    unexpectedly return a number left to the reference.
-    """
-    mylist = np.arange(0, 25)
-    for i in range(0, len(mylist)):
-        res = m.takeclosestright(mylist, i)
-        if i == 0:
-            assert res == 1
-        elif i == len(mylist) - 1:
-            assert res == 24
-        else:
-            assert res == i + 1
-
-
-def test_closestleft():
-    """
-    Tests whether closestright function works as intended and does not
-    unexpectedly return a number right to the reference.
-    """
-    mylist = np.arange(-1, 25)
-    for i in range(0, len(mylist)):
-        res = m.takeclosestleft(mylist, i)
-        if i == 0:
-            assert res == mylist[0]
-        elif i == len(mylist) - 1:
-            assert res == 24
-        elif i == len(mylist):
-            assert res == 25
-        else:
-            assert res == i - 1
-
-
-def test_createchunks(run=1, subj=1):
-    """
-    Tests chunking of studyforrest data into scenes based on shot annotation.
-    Are start and end ids of shots the same length?
-    Does no endid preceed a start id?
-    run: int
-        specify the run (choose between 1 - 8)
-    subj: int
-        specify the subject (example data of lab subject (1)
-        or mri subject (2) available)
-    """
-    data1, data2, shots = ut.same_sample(run, subj)
-    onsets = m.create_onsets(shots, 3)
-    fixations = m.preprocess(data1)
-    startid, endid = m.create_chunks(onsets, fixations, 3)
-    assert len(startid) == len(endid)
-    trues = []
-    for i in range(0, len(startid)):
-        trues.append(startid[i] <= endid[i])
-    assert all(trues)
-
-
-def test_createoffsetchunks(run=1, subj=1):
-    """
-    Tests chunking of studyforrest data into scenes based on shot annotation.
-    Are start and end ids of shots the same length?
-    Does no endid preceed a start id?
-    run: int
-        specify the run (choose between 1 - 8)
-    subj: int
-        specify the subject (example data of lab subject (1)
-        or mri subject (2) available)
-    """
-    data1, data2, shots = ut.same_sample(run, subj)
-    offsets = m.create_offsets(shots, 3)
-    fixations = m.preprocess(data1)
-    startid, endid = m.create_offsetchunks(offsets, fixations, 3)
-    assert len(startid) == len(endid)
-    trues = []
-    for i in range(0, len(startid)):
-        trues.append(startid[i] <= endid[i])
-    assert all(trues)
-
-
-def test_longshot():
-    """
-    Test whether longshots combines scenes of longer length into the
-    correct number of shots.
-    """
-    shots = ut.short_shots()
-    newshots = m.longshot(shots, group_shots=True, ldur=4.92)
-    assert len(newshots) == len(shots) - 2
-
-
-def test_longshot_nogrouping():
-    """
-    Tests whether longshots will perform no grouping if dur = None.
-    """
-    shots = ut.short_shots()
-    newshots = m.longshot(shots, group_shots=False, ldur=0.0)
-    assert len(newshots) == len(shots)
-
-
 def test_compare2matlab():
     """
     compares whether analysis with given inputs yields the same results as a
@@ -292,6 +196,23 @@ def test_compare2matlab():
     assert matlab_no_grouping == approx(res_no_grouping[0], abs=1e-5, rel=1e-5)
 
 
+def test_too_short_scanpaths():
+    """
+    if at least one scanpath is too short results should be nan.
+    """
+    fixvector1 = ut.mk_fix_vector(2)
+    fixvector2 = ut.mk_fix_vector(5)
+    results = mp.docomparison(fixvector1, fixvector2, sz=[1280, 720], grouping=True, TAmp=8, TDir=8, TDur=8)
+    assert ([results[i] == np.nan for i in range(0, len(results))])
+
+
+# The original use case of this module was for a Masters Thesis that performed
+# scanpath comparison on the studyforrest dataset (Hanke et al., 2016). The test
+# in this sections utilize (and test) functions that are related to this data
+# specifically (found under tests/testdata and in a non-entrypoint script
+# multimatch_forrest). Coincidentally, these tests test multimatch functions
+# nevertheless, so they'll stay for now..
+
 def test_offsets():
     """
     Tests offset length on example dataset with known results.
@@ -303,16 +224,100 @@ def test_offsets():
     shots_short = ut.mk_supershort_shots()
     offsets2 = m.create_offsets(shots_short, dur=2)
     assert len(offsets2) == 0
+
     shots_long = ut.mk_longershots()
     offsets3 = m.create_offsets(shots_long, dur=3)
     assert len(offsets3) == len(shots_long)
 
 
-def test_too_short_scanpaths():
+# disabling those for now -- chunking is not relevant for multimatch anymore
+
+# def test_createchunks(run=1, subj=1):
+#     """
+#     Tests chunking of studyforrest data into scenes based on shot annotation.
+#     Are start and end ids of shots the same length?
+#     Does no endid preceed a start id?
+#     run: int
+#         specify the run (choose between 1 - 8)
+#     subj: int
+#         specify the subject (example data of lab subject (1)
+#         or mri subject (2) available)
+#     """
+#     data1, data2, shots = ut.same_sample(run, subj)
+#     onsets = m.create_onsets(shots, 3)
+#     fixations = m.preprocess(data1)
+#     startid, endid = m.create_chunks(onsets, fixations, 3)
+#     assert len(startid) == len(endid)
+#     assert np.all(startid[:-1] <= endid[1:])
+#
+#
+# def test_createoffsetchunks(run=1, subj=1):
+#     """
+#     Tests chunking of studyforrest data into scenes based on shot annotation.
+#     Are start and end ids of shots the same length?
+#     Does no endid preceed a start id?
+#     run: int
+#         specify the run (choose between 1 - 8)
+#     subj: int
+#         specify the subject (example data of lab subject (1)
+#         or mri subject (2) available)
+#     """
+#     data1, data2, shots = ut.same_sample(run, subj)
+#     offsets = m.create_offsets(shots, 3)
+#     fixations = m.preprocess(data1)
+#     startid, endid = m.create_offsetchunks(offsets, fixations, 3)
+#     assert len(startid) == len(endid)
+#     assert np.all(startid[:-1] <= endid[1:])
+
+
+def test_longshot():
     """
-    if at least one scanpath is too short results should be nan.
+    Test whether longshots combines scenes of longer length into the
+    correct number of shots.
     """
-    fixvector1 = ut.mk_fix_vector(2)
-    fixvector2 = ut.mk_fix_vector(5)
-    results = mp.docomparison(fixvector1, fixvector2, sz=[1280, 720], grouping=True, TAmp=8, TDir=8, TDur=8)
-    assert ([results[i] == np.nan for i in range(0, len(results))])
+    shots = ut.short_shots()
+    newshots = m.longshot(shots, group_shots=True, ldur=4.92)
+    assert len(newshots) == len(shots) - 2
+
+
+def test_longshot_nogrouping():
+    """
+    Tests whether longshots will perform no grouping if dur = None.
+    """
+    shots = ut.short_shots()
+    newshots = m.longshot(shots, group_shots=False, ldur=0.0)
+    assert len(newshots) == len(shots)
+
+
+def test_closestright():
+    """
+    Tests whether closestright function works as intended and does not
+    unexpectedly return a number left to the reference.
+    """
+    mylist = np.arange(0, 25)
+    for i in range(0, len(mylist)):
+        res = m.takeclosestright(mylist, i)
+        if i == 0:
+            assert res == 1
+        elif i == len(mylist) - 1:
+            assert res == 24
+        else:
+            assert res == i + 1
+
+
+def test_closestleft():
+    """
+    Tests whether closestright function works as intended and does not
+    unexpectedly return a number right to the reference.
+    """
+    mylist = np.arange(-1, 25)
+    for i in range(0, len(mylist)):
+        res = m.takeclosestleft(mylist, i)
+        if i == 0:
+            assert res == mylist[0]
+        elif i == len(mylist) - 1:
+            assert res == 24
+        elif i == len(mylist):
+            assert res == 25
+        else:
+            assert res == i - 1
